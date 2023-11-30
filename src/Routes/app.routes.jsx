e25024715365d1eb4837ba8c1d8b9235a0e51d39
Morgana Ramos <morganaramos@edu.unifor.br>
2023-11-30 19:29:53 -0300

    incluindo rotas de curso e grupo

--- src/Routes/app.routes.jsx
+++ src/Routes/app.routes.jsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from "react-router-dom";
-
+import { TelaCurso } from "../pages/TelaCurso";
 import { ChatPage } from "../pages/ChatPage";
+import { GrupoSelecto } from "../pages/GrupoSelecto";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<ChatPage />} />
+      <Route path="/curso" element={<TelaCurso />}/>
+      <Route path="/grupo" element={<GrupoSelecto />}/>
     </Routes>
   );
 }
